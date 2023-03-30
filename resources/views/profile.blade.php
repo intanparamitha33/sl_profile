f2c4b8ed8b768facfbb7fb231f75a01ef5e8b3dc
intanparamitha33 <intanparamitha33@gmail.com>
2023-03-31 00:21:15 +0700

    Membuat NavBar dan Mulai Section 1

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,8 +10,34 @@
     <script src="node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
     <script src="node_modules/@popperjs/core/dist/umd/popper.js"></script>
     <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="/assets/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,800;1,900&display=swap" rel="stylesheet">
+
+    
 </head>
 <body>
-    
+    <div class="main">
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#short-intro">Short Intro</a>
+              <a class="navbar-brand" href="#">Hobbies</a>
+              <a class="navbar-brand" href="#">Skills</a>
+              <a class="navbar-brand" href="#">Life Experiences</a>
+              <a class="navbar-brand" href="#">Lovers</a>
+            </div>
+
+            <div class="line-under-navtext" style="border: 0.11vw solid black"></div>
+        </nav>
+
+        <div class="short-intro">   
+            <div class="my-photo">
+                <div class="photo-holder"></div>
+                <div class="photo-only">
+                    <img class="the-photo" src="assets/my_photo.jpg" alt="Author's Photo">
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
