6a97e0229b57ccc2c78bc3ed0889d0a7e176eb95
intanparamitha33 <intanparamitha33@gmail.com>
2023-03-31 23:33:56 +0700

    Membuat konten section 2 bagian 1

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -26,7 +26,7 @@
           <div class="collapse navbar-collapse" id="navbarNav"> --}}
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" style="color: antiquewhite" href="#short-intro">Short Intro</a>
+                <a class="nav-link active" aria-current="page" style="color: antiquewhite" href="#about-me">About Me</a>
               </li>  
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" style="color: rgb(100, 194, 249)" href="#hobbies">Hobbies</a>
@@ -61,51 +61,55 @@
         </div>
     </div>
 
-    <div class="short-intro">
-      <h2>
-        Some things
-        <small class="text-body-secondary" style="opacity: 70%">you need to know</small>
-        about me
-      </h2>
-      <dl class="row">
-        <dt class="col-sm-3">Description lists</dt>
-        <dd class="col-sm-9">A description list is perfect for defining terms.</dd>
-      
-        <dt class="col-sm-3">Term</dt>
-        <dd class="col-sm-9">
-          <p>Definition for the term.</p>
-          <p>And some more placeholder definition text.</p>
-        </dd>
-      
-        <dt class="col-sm-3">Another term</dt>
-        <dd class="col-sm-9">This definition is short, so no extra paragraphs or anything.</dd>
-      
-        <dt class="col-sm-3 text-truncate">Truncated term is truncated</dt>
-        <dd class="col-sm-9">This can be useful when space is tight. Adds an ellipsis at the end.</dd>
-      
-        <dt class="col-sm-3">Nesting</dt>
-        <dd class="col-sm-9">
-          <dl class="row">
-            <dt class="col-sm-4">Nested definition list</dt>
-            <dd class="col-sm-8">I heard you like definition lists. Let me put a definition list inside your definition list.</dd>
-          </dl>
-        </dd>
-      </dl>
+    <div id="about-me">
+      <div class="line-container" style="display: flex; justify-content: space-around; margin: 3vw 0vw">
+        <div class="line-1"></div>
+      </div>
+      <div class="intro-desc">
+        <h2 style="font-weight: 700">
+          Some things
+          <small class="text-body-secondary" style="opacity: 60%">you need to know</small>
+          about me
+        </h2>
+        <ul class="list-unstyled" style="font-weight: 500; font-size: 1.2vw">
+          <div class="amdesc-sect1">
+            <li>My name is <span id="span-text">Intan Paramitha</span>. My full name is quite long, but my nickname is so simple, <span id="span-text">IP</span>.</li>
+            <li>
+              I just turned <span id="span-text">20 years old</span> this year, precisely on <span id="span-text">March 27th</span>.
+              <br>
+              Just now, I already told you about my birthday!
+            </li>
+          </div>
+          <br>
+          
+          <div class="amdesc-sect3"> 
+            <li>However, this style only applies to immediate child elements.</li>
+            <li>Nested lists:
+              <ul>
+                <li>are unaffected by this style</li>
+                <li>will still show a bullet</li>
+                <li>and have appropriate left margin</li>
+              </ul>
+            </li>
+            <li>This may still come in handy in some situations.</li>
+          </div>
+        </ul>
+      </div>
     </div>
     
-    <div class="hobbies">
+    <div id="hobbies">
 
     </div>
 
-    <div class="skills">
+    <div id="skills">
 
     </div>
 
-    <div class="experiences">
+    <div id="experiences">
 
     </div>
 
-    <div class="lovers">
+    <div id="lovers">
 
     </div>
 </body>
